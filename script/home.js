bf59ea80af9d96e06ee6624c788ac0f04d730c74
mostafiz365 <rahmanmostafiz365@gmail.com>
2026-03-09 04:01:50 +0600

    sovled some issues and added comments

--- script/home.js
+++ script/home.js
@@ -121,21 +121,8 @@ const loadIssuesCard = () => {
         displayIssuesCard(data.data);
     })
 };
-// {
-// "id": 1,
-// "title": "Fix navigation menu on mobile devices",
-// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-// "status": "open",
-// "labels": [
-// "bug",
-// "help wanted"
-// ],
-// "priority": "high",
-// "author": "john_doe",
-// "assignee": "jane_smith",
-// "createdAt": "2024-01-15T10:30:00Z",
-// "updatedAt": "2024-01-15T10:30:00Z"
-// },
+
+// Show Modal Function ---------
 const loadIssuesDetail = async (id) => {
     const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
     const res = await fetch(url);
@@ -150,7 +137,7 @@ const displayIssuesDetails = (card) =>{
     <h4 class="font-semibold text-[16px] text-[#1F2937]"> ${card.title} </h4>
     <div class="flex gap-3 items-center">
     <p class="${card.status === 'open' ? 'badge bg-green-500 text-white' : 'badge bg-purple-500 text-white'}">${card.status}</p>
-    <p class="text-[12px] text-[#64748B]">${card.assignee}</p>
+    <p class="text-[12px] text-[#64748B]">${card.author}</p>
     <p class="text-[12px] text-[#64748B]">${card.updatedAt}</p>
     </div>
     <div>${createElement(card.labels)}</div>
@@ -200,7 +187,7 @@ const displayIssuesCard = (cards) => {
 };
 loadIssuesCard();
 
-
+// Search Option------
 document.getElementById('btn-search').addEventListener('click', () =>{
     const inputSearch = document.getElementById('input-search');
     const searchValue = inputSearch.value.trim().toLowerCase();
